d19fc5599ec36251914c7e2dd56d0a73f9ae6d0a
urvashi-code1255 <urvashianand0906@gmail.com>
2020-07-03 20:24:57 +0530

    l

---
+++ Day-3/Prison_Cells_After_N_Days.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        vector<int>temp(8);
+        
+        // as the cycle is repeating after 14 days , so if no of days is multiple of 14 then we have to consider  
+	    //only upto first 14 days because after that ,values will repeat .
+        N=(N%14 == 0)?14:N%14;  
+        
+        while(N--)
+        {
+            for(int i = 1 ; i < 7 ; i++)  
+            {
+                temp[i]=(cells[i-1]==cells[i+1])?1:0; // checking equality of adjacent cells.
+            }
+            cells = temp;
+        }
+        return cells;
+    }
+};
