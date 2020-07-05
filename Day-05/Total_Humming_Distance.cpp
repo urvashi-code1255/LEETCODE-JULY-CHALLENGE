a3d20cab8e31914cf844c45b754eecddfccb8630
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-07-05 16:53:16 +0530

    Total_Humming_Distance.cpp

--- Day-05/Total_Humming_Distance.cpp
+++ Day-05/Total_Humming_Distance.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-       int result = 0 ;
-        for(int i = 0 ; i < 32 ; i++)
-        {   
-            int ones = 0 , zero = 0 ;
-            for(auto j : nums)
-            {
-                if(j & (1 << i ))
-                    ones ++ ;
-                else
-                    zero ++ ;  
-            }
+        int result = 0 ;
+        for(int i = 0 ; i < 32 ; i++)                      // for example - 2 , 14 , 4 
+        {                                                  // 2 -  0010
+            int ones = 0 , zero = 0 ;                      // 14 - 1110
+            for(auto j : nums)                             // 4 -  0100
+            {                                              // at first position in all ,ones = 0 , zeroes = 3 
+                if(j & (1 << i ))                          // at second position in all , ones = 2 , zeroes = 1 
+                    ones ++ ;                              // at third position in all , ones = 2 , zeroes = 1 
+                else                                       // at fourth position in all , ones = 1 , zeroes = 2 
+                    zero ++ ;                              // result = 0*3 + 2*1 + 2*1 + 1*2 = 6
+            }                                              // keep in mind positions are taken from end .
            result += ones*(zero) ;
         } 
         return result;
