0e7d0c6b182fa9823ac51f131ce4b199ebf9eee9
urvashi-code1255 <urvashianand0906@gmail.com>
2020-07-07 21:19:43 +0530

    l

---
+++ Day-07/Island_Perimeter-right-left.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter = 0 , m = grid.size() , n = grid[0].size();
+        
+        for (int i = 0 ; i < m ; i++) 
+        {
+            for (int j = 0 ; j < n ; j++) 
+            {
+                if (grid[i][j] == 1) 
+                {
+                    perimeter += 4;
+                    if (i < m - 1) 
+                        perimeter -= 2 * grid[i + 1][j];
+                    if (j < n - 1) 
+                        perimeter -= 2 * grid[i][j + 1];
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};
