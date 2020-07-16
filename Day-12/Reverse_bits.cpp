8e0f40e69805685b9934189c21528126d17a6a8f
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-07-16 17:50:02 +0530

    fine

--- Day-12/Reverse_bits.cpp
+++ Day-12/Reverse_bits.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
+    uint32_t reverseBits(uint32_t n) 
+    {
         uint32_t reverse = 0;
         for (int i = 0; i < 32; i++) {
             reverse = (reverse << 1) | (n & 1); 
