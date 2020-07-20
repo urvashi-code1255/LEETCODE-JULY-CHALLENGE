8dbecf40d2298198d2f93f6e083b4f896d3c5fe2
urvashi-code1255 <urvashianand0906@gmail.com>
2020-07-20 19:13:16 +0530

    day-19,20

---
+++ Day-19/Add_Binary.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = a.length() - 1 , j = b.length() - 1 , carry = 0 ;
+        string str="";
+        while(i >= 0 || j >= 0 || carry)
+        {
+            int sum = ((i >= 0) ? (a[i] - '0') : 0) + ((j >= 0) ? (b[j] - '0') : 0) + carry ;
+            carry = sum/2 ;
+            str = char((sum%2) + '0') + str;
+            i--; j--;
+        }
+        return str;
+    }
+};
---
+++ Day-20/Remove_linked_list_Elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while(head && head->val==val)
+        {
+            head = head->next;
+        }
+        ListNode* current_node = head;
+        while(current_node && current_node->next)
+        {
+            if(current_node->next->val==val)
+            {
+                current_node->next=current_node->next->next;
+            }
+            else
+            {
+                current_node = current_node->next;
+            }
+        }
+        return head;
+    }
+};
