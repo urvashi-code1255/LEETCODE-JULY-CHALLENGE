d49a27e085c551a0ae7023ce8a1e87ed660a495a
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-07-27 17:37:02 +0530

    done

--- Day-27/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
+++ Day-27/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
@@ -5,8 +5,7 @@ public:
         return Tree(inorder , postorder , 0 , inorder.size()-1 , 0 , postorder.size()-1);
     }
     
-    TreeNode* Tree(vector<int>& inorder, vector<int>& postorder , int iBegin , int iEnd , int pBegin ,int pEnd )
-    {
+    TreeNode* Tree(vector<int>& inorder, vector<int>& postorder , int iBegin , int iEnd , int pBegin ,int pEnd ){
         if(iBegin>iEnd || pBegin>pEnd)
             return NULL ;
         
@@ -17,6 +16,7 @@ public:
             i++;
         
         TreeNode* t = new TreeNode(val);
+        
         t->left = Tree( inorder ,postorder, iBegin , i-1 , pBegin , pBegin+(i-1-iBegin));
         t->right = Tree(inorder, postorder , i+1 , iEnd , pEnd-(iEnd-i) , pEnd-1);
         return t;
